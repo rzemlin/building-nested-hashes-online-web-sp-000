2056f860f80870535a48c4e4ebdd0eaf4b1634a3
Ryan Zemlin <rzemlin@gmail.com>
2021-02-21 14:56:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,32 +2,32 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord montague"
+        :name => "Lord montague",
         :age => "53"
         },
       :matriarch => {
-        :name => "lady montague"
+        :name => "lady montague",
         :age => "54"
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord capulet"
+        :name => "Lord capulet",
         :age => "50"
         },
       :matriarch => {
-        :name => "lady capulet"
+        :name => "lady capulet",
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
